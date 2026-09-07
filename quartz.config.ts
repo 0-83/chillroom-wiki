7f7a54e16f354a0a0b60e28ca10aa471a55f3af9
iSp1r1t868 <vladislavonkov2007@gmail.com>
2026-09-08 00:04:33 +0500

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "ChillRoom Wiki",
+    pageTitle: "🪴 Quartz 4.0",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
